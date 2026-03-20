fab9e7d5c6413cdc1efab4367d7a8577505c9f69
MyCometG3 <mycometg3@icloud.com>
2026-03-20 18:05:34 +0900

    Create qlstephenswift.rb file

---
+++ Casks/qlstephenswift.rb
@@ -0,0 +1,11 @@
+cask "qlstephenswift" do
+  version "20260206"
+  sha256 "54025dcadc9d66936a4b8a5cea56c87b7144039ff2c64cb5e6b428bc3491d5eb"
+
+  url "https://github.com/MyCometG3/QLStephenSwift/releases/download/20260206/QLStephenSwift.20260206.dmg"
+  name "QLStephenSwift"
+  desc "Quick Look plugin app"
+  homepage "https://github.com/MyCometG3/QLStephenSwift"
+
+  app "QLStephenSwift.app"
+end
